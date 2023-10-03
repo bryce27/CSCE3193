964c666b1769263924388f45f24c57a3e54baa66
Bryce <byoung888@gmail.com>
2023-10-03 15:51:25 -0500

    step 3 part 1

--- proj3/turtle.js
+++ proj3/turtle.js
@@ -1,168 +1,148 @@
-class Sprite {
-	constructor(x, y, image_url, update_method, onclick_method) {
-		this.x = x;
-		this.y = y;
+"use strict";
+var Sprite = /** @class */ (function () {
+    function Sprite(x, y, image_url, update_method, onclick_method) {
+        this.x = x;
+        this.y = y;
         this.speed = 4;
-		this.image = new Image();
-		this.image.src = image_url;
-		this.update = update_method;
-		this.onclick = onclick_method;
-	}
-
-	set_destination(x, y) {
-		this.dest_x = x;
-		this.dest_y = y;
-	}
-
-	ignore_click(x, y) {
-	}
-
-	move(dx, dy) {
-		this.dest_x = this.x + dx;
-		this.dest_y = this.y + dy;
-	}
-
-	go_toward_destination() {
-		if(this.dest_x === undefined)
-			return;
-
-		if(this.x < this.dest_x)
-			this.x += Math.min(this.dest_x - this.x, this.speed);
-		else if(this.x > this.dest_x)
-			this.x -= Math.min(this.x - this.dest_x, this.speed);
-		if(this.y < this.dest_y)
-			this.y += Math.min(this.dest_y - this.y, this.speed);
-		else if(this.y > this.dest_y)
-			this.y -= Math.min(this.y - this.dest_y, this.speed);
-	}
-
-	sit_still() {
-	}
-}
-
-
-
-
-
-
-class Model {
-	constructor() {
-		this.sprites = [];
-		this.sprites.push(new Sprite(200, 100, "lettuce.png", Sprite.prototype.sit_still, Sprite.prototype.ignore_click));
-		this.turtle = new Sprite(50, 50, "turtle.png", Sprite.prototype.go_toward_destination, Sprite.prototype.set_destination);
-		this.sprites.push(this.turtle);
-	}
-
-	update() {
-		for (const sprite of this.sprites) {
-			sprite.update();
-		}
-	}
-
-	onclick(x, y) {
-		for (const sprite of this.sprites) {
-			sprite.onclick(x, y);
-		}
-	}
-
-	move(dx, dy) {
-		this.turtle.move(dx, dy);
-	}
-}
-
-
-
-
-class View
-{
-	constructor(model) {
-		this.model = model;
-		this.canvas = document.getElementById("myCanvas");
-		this.turtle = new Image();
-		this.turtle.src = "turtle.png";
-	}
-
-	update() {
-		let ctx = this.canvas.getContext("2d");
-		ctx.clearRect(0, 0, 1000, 500);
-		for (const sprite of this.model.sprites) {
-			ctx.drawImage(sprite.image, sprite.x - sprite.image.width / 2, sprite.y - sprite.image.height);
-		}
-	}
-}
-
-
-
-
-
-
-
-class Controller
-{
-	constructor(model, view) {
-		this.model = model;
-		this.view = view;
-		this.key_right = false;
-		this.key_left = false;
-		this.key_up = false;
-		this.key_down = false;
-		let self = this;
-		view.canvas.addEventListener("click", function(event) { self.onClick(event); });
-		document.addEventListener('keydown', function(event) { self.keyDown(event); }, false);
-		document.addEventListener('keyup', function(event) { self.keyUp(event); }, false);
-	}
-
-	onClick(event) {
-		const x = event.pageX - this.view.canvas.offsetLeft;
-		const y = event.pageY - this.view.canvas.offsetTop;
-		this.model.onclick(x, y);
-	}
-
-	keyDown(event) {
-		if(event.keyCode == 39) this.key_right = true;
-		else if(event.keyCode == 37) this.key_left = true;
-		else if(event.keyCode == 38) this.key_up = true;
-		else if(event.keyCode == 40) this.key_down = true;
-	}
-
-	keyUp(event) {
-		if(event.keyCode == 39) this.key_right = false;
-		else if(event.keyCode == 37) this.key_left = false;
-		else if(event.keyCode == 38) this.key_up = false;
-		else if(event.keyCode == 40) this.key_down = false;
-	}
-
-	update() {
-		let dx = 0;
-		let dy = 0;
-        let speed = this.model.turtle.speed;
-		if(this.key_right) dx += speed;
-		if(this.key_left) dx -= speed;
-		if(this.key_up) dy -= speed;
-		if(this.key_down) dy += speed;
-		if(dx != 0 || dy != 0)
-			this.model.move(dx, dy);
-	}
-}
-
-
-
-
-
-class Game {
-	constructor() {
-		this.model = new Model();
-		this.view = new View(this.model);
-		this.controller = new Controller(this.model, this.view);
-	}
-
-	onTimer() {
-		this.controller.update();
-		this.model.update();
-		this.view.update();
-	}
-}
-
-
-let game = new Game();
-let timer = setInterval(() => { game.onTimer(); }, 40);
+        this.image = new Image();
+        this.image.src = image_url;
+        this.update = update_method;
+        this.onclick = onclick_method;
+    }
+    Sprite.prototype.set_destination = function (x, y) {
+        this.dest_x = x;
+        this.dest_y = y;
+    };
+    Sprite.prototype.ignore_click = function (x, y) {
+    };
+    Sprite.prototype.move = function (dx, dy) {
+        this.dest_x = this.x + dx;
+        this.dest_y = this.y + dy;
+    };
+    Sprite.prototype.go_toward_destination = function () {
+        if (this.dest_x === undefined)
+            return;
+        if (this.x < this.dest_x)
+            this.x += Math.min(this.dest_x - this.x, this.speed);
+        else if (this.x > this.dest_x)
+            this.x -= Math.min(this.x - this.dest_x, this.speed);
+        if (this.y < this.dest_y)
+            this.y += Math.min(this.dest_y - this.y, this.speed);
+        else if (this.y > this.dest_y)
+            this.y -= Math.min(this.y - this.dest_y, this.speed);
+    };
+    Sprite.prototype.sit_still = function () {
+    };
+    return Sprite;
+}());
+var Model = /** @class */ (function () {
+    function Model() {
+        this.sprites = [];
+        this.sprites.push(new Sprite(200, 100, "lettuce.png", Sprite.prototype.sit_still, Sprite.prototype.ignore_click));
+        this.turtle = new Sprite(50, 50, "turtle.png", Sprite.prototype.go_toward_destination, Sprite.prototype.set_destination);
+        this.sprites.push(this.turtle);
+    }
+    Model.prototype.update = function () {
+        for (var _i = 0, _a = this.sprites; _i < _a.length; _i++) {
+            var sprite = _a[_i];
+            sprite.update();
+        }
+    };
+    Model.prototype.onclick = function (x, y) {
+        for (var _i = 0, _a = this.sprites; _i < _a.length; _i++) {
+            var sprite = _a[_i];
+            sprite.onclick(x, y);
+        }
+    };
+    Model.prototype.move = function (dx, dy) {
+        this.turtle.move(dx, dy);
+    };
+    return Model;
+}());
+var View = /** @class */ (function () {
+    function View(model) {
+        this.model = model;
+        this.canvas = document.getElementById("myCanvas");
+        this.turtle = new Image();
+        this.turtle.src = "turtle.png";
+    }
+    View.prototype.update = function () {
+        var ctx = this.canvas.getContext("2d");
+        ctx.clearRect(0, 0, 1000, 500);
+        for (var _i = 0, _a = this.model.sprites; _i < _a.length; _i++) {
+            var sprite = _a[_i];
+            ctx.drawImage(sprite.image, sprite.x - sprite.image.width / 2, sprite.y - sprite.image.height);
+        }
+    };
+    return View;
+}());
+var Controller = /** @class */ (function () {
+    function Controller(model, view) {
+        this.model = model;
+        this.view = view;
+        this.key_right = false;
+        this.key_left = false;
+        this.key_up = false;
+        this.key_down = false;
+        var self = this;
+        view.canvas.addEventListener("click", function (event) { self.onClick(event); });
+        document.addEventListener('keydown', function (event) { self.keyDown(event); }, false);
+        document.addEventListener('keyup', function (event) { self.keyUp(event); }, false);
+    }
+    Controller.prototype.onClick = function (event) {
+        var x = event.pageX - this.view.canvas.offsetLeft;
+        var y = event.pageY - this.view.canvas.offsetTop;
+        this.model.onclick(x, y);
+    };
+    Controller.prototype.keyDown = function (event) {
+        if (event.keyCode == 39)
+            this.key_right = true;
+        else if (event.keyCode == 37)
+            this.key_left = true;
+        else if (event.keyCode == 38)
+            this.key_up = true;
+        else if (event.keyCode == 40)
+            this.key_down = true;
+    };
+    Controller.prototype.keyUp = function (event) {
+        if (event.keyCode == 39)
+            this.key_right = false;
+        else if (event.keyCode == 37)
+            this.key_left = false;
+        else if (event.keyCode == 38)
+            this.key_up = false;
+        else if (event.keyCode == 40)
+            this.key_down = false;
+    };
+    Controller.prototype.update = function () {
+        var dx = 0;
+        var dy = 0;
+        var speed = this.model.turtle.speed;
+        if (this.key_right)
+            dx += speed;
+        if (this.key_left)
+            dx -= speed;
+        if (this.key_up)
+            dy -= speed;
+        if (this.key_down)
+            dy += speed;
+        if (dx != 0 || dy != 0)
+            this.model.move(dx, dy);
+    };
+    return Controller;
+}());
+var Game = /** @class */ (function () {
+    function Game() {
+        this.model = new Model();
+        this.view = new View(this.model);
+        this.controller = new Controller(this.model, this.view);
+    }
+    Game.prototype.onTimer = function () {
+        this.controller.update();
+        this.model.update();
+        this.view.update();
+    };
+    return Game;
+}());
+var game = new Game();
+var timer = setInterval(function () { game.onTimer(); }, 40);

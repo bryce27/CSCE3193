bad20272ab26427e439bf5b8685e51d1a64cd6a4
Bryce <byoung888@gmail.com>
2023-11-10 04:17:13 -0600

    proj4 working and submitted

--- proj4/back_end/main.py
+++ proj4/back_end/main.py
@@ -1,23 +1,74 @@
 from typing import Mapping, Any
 import os
 from http_daemon import delay_open_url, serve_pages
+from typing import Dict, List, Tuple
 
-def make_ajax_page(params: Mapping[str, Any]) -> Mapping[str, Any]:
-    print(f'make_ajax_page was called with {params}')
-    return {
-        'message': 'yo momma',
-    }
+class Player:
+    def __init__(self, id:str) -> None:
+        self.id = id
+        self.x = 0
+        self.y = 0
+        self.what_i_know = 0 
+
+players: Dict[str, Player] = {}
+history: List[Tuple[str, int, int]] = []
+
+
+def find_or_create_player(id:str) -> Any:
+
+    if players.get(id) is None:
+        new_player = Player(id)
+        players[id] = new_player
+
+        return new_player
+    else:
+        return players[id]
+    
+
+def ajax(req: Mapping[str, Any]) -> Mapping[str, Any]:
+
+    if req['action'] == 'click':
+        player = find_or_create_player(req['id'])
+        player.x = req['x']
+        player.y = req['y']
+
+        history.append((player.id, player.x, player.y))
+
+        return {
+            'message': 'OK',
+        }
+    
+    elif req['action'] == 'update': 
+        if (len(history) == 0):
+            return {
+                'message': 'success',
+                'updates': history
+            }
+        else:
+            player = find_or_create_player(req['id'])
+            what_they_knew = player.what_i_know
+            player.what_i_know = len(history)
+
+            return {
+                'message': 'success',
+                'updates': history[what_they_knew:player.what_i_know]
+            }
+
+    else:
+        raise ValueError('we shouldnt be here')
+    
 
 def main() -> None:
-    # Get set up
     os.chdir(os.path.join(os.path.dirname(__file__), '../front_end'))
 
-    # Serve pages
+    # Routes
     port = 8987
     delay_open_url(f'http://localhost:{port}/game.html', .1)
-    serve_pages(port, {
-        'ajax.html': make_ajax_page,
+    # delay_open_url(f'http://localhost:{port}/game.html', .2) # for second browser window
+    serve_pages(port, { 
+        'ajax': ajax,
     })
+
 
 if __name__ == "__main__":
     main()
--- proj4/front_end/main.ts
+++ proj4/front_end/main.ts
@@ -14,6 +14,8 @@ class Sprite {
 	constructor(x: number, y: number, image_url: string, update_method: UpdateMethod, onclick_method: OnClickMethod) {
 		this.x = x;
 		this.y = y;
+		this.dest_x = x;
+		this.dest_y = y;
         this.speed = 4;
 		this.image = new Image();
 		this.image.src = image_url;
@@ -26,8 +28,7 @@ class Sprite {
 		this.dest_y = y;
 	}
 
-	ignore_click(x: number, y: number) {
-	}
+	ignore_click(x: number, y: number) {}
 
 	move(dx: number, dy: number) {
 		this.dest_x = this.x + dx;
@@ -41,30 +42,47 @@ class Sprite {
 		if(this.dest_y === undefined)
 			return;
 
-		if(this.x < this.dest_x)
-			this.x += Math.min(this.dest_x - this.x, this.speed);
-		else if(this.x > this.dest_x)
-			this.x -= Math.min(this.x - this.dest_x, this.speed);
-		if(this.y < this.dest_y)
-			this.y += Math.min(this.dest_y - this.y, this.speed);
-		else if(this.y > this.dest_y)
-			this.y -= Math.min(this.y - this.dest_y, this.speed);
+		let dist_remaining = Math.sqrt((this.dest_x - this.x) * (this.dest_x - this.x) + (this.dest_y - this.y) * (this.dest_y - this.y));
+		let step_size = Math.min(2.5, dist_remaining);
+		let angle = Math.atan2(this.dest_y - this.y, this.dest_x - this.x)
+		this.x += step_size * Math.cos(angle);
+		this.y += step_size * Math.sin(angle);
+
+		// old way
+		// if(this.x < this.dest_x)
+		// 	this.x += Math.min(this.dest_x - this.x, this.speed);
+		// else if(this.x > this.dest_x)
+		// 	this.x -= Math.min(this.x - this.dest_x, this.speed);
+		// if(this.y < this.dest_y)
+		// 	this.y += Math.min(this.dest_y - this.y, this.speed);
+		// else if(this.y > this.dest_y)
+		// 	this.y -= Math.min(this.y - this.dest_y, this.speed);
 	}
 
-	sit_still() {
-	}
+	sit_still() {}
 }
 
+// map IDs to sprites
+let sprite_map: Record<string, Sprite> = {};
+
+const random_id = (len:number) => { // for ID generation
+    let p = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    return [...Array(len)].reduce(a => a + p[Math.floor(Math.random() * p.length)], '');
+}
+
+const g_origin = new URL(window.location.href).origin;
+const g_id = random_id(12);
 
 class Model {
 	sprites: Sprite[] = [];
-	turtle: Sprite;
+	character: Sprite;
 
 	constructor() {
 		this.sprites = [];
 		this.sprites.push(new Sprite(200, 100, "lettuce.png", Sprite.prototype.sit_still, Sprite.prototype.ignore_click));
-		this.turtle = new Sprite(50, 50, "turtle.png", Sprite.prototype.go_toward_destination, Sprite.prototype.set_destination);
-		this.sprites.push(this.turtle);
+		this.character = new Sprite(50, 50, "blue_robot.png", Sprite.prototype.go_toward_destination, Sprite.prototype.set_destination);
+		this.sprites.push(this.character);
+		sprite_map[g_id] = this.character;
 	}
 
 	update() {
@@ -80,7 +98,7 @@ class Model {
 	}
 
 	move(dx: number, dy: number) {
-		this.turtle.move(dx, dy);
+		this.character.move(dx, dy);
 	}
 }
 
@@ -89,13 +107,13 @@ class View
 {
 	model: Model;
     canvas: HTMLCanvasElement;
-    turtle: HTMLImageElement;
+    character: HTMLImageElement;
 
 	constructor(model: Model) {
 		this.model = model;
 		this.canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
-		this.turtle = new Image();
-		this.turtle.src = "turtle.png";
+		this.character = new Image();
+		this.character.src = "blue_robot.png";
 	}
 
 	update() {
@@ -107,108 +125,10 @@ class View
 	}
 }
 
-
-class Controller
-{
-	model: Model;
-    view: View;
-    key_right: boolean = false;
-    key_left: boolean = false;
-    key_up: boolean = false;
-    key_down: boolean = false;
-
-	constructor(model: Model, view: View) {
-		this.model = model;
-		this.view = view;
-		this.key_right = false;
-		this.key_left = false;
-		this.key_up = false;
-		this.key_down = false;
-		let self = this;
-		view.canvas.addEventListener("click", function(event) { self.onClick(event); });
-		document.addEventListener('keydown', function(event) { self.keyDown(event); }, false);
-		document.addEventListener('keyup', function(event) { self.keyUp(event); }, false);
-	}
-
-	onClick(event: MouseEvent) {
-		let x = event.pageX - this.view.canvas.offsetLeft;
-		let y = event.pageY - this.view.canvas.offsetTop;
-		this.model.onclick(x, y);
-
-		httpPost('ajax.html', {
-			id: g_id,
-			action: 'move',
-			x: x,
-			y: y,
-		}, this.onAcknowledgeClick);
-	}
-
-	keyDown(event: KeyboardEvent) {
-		if(event.keyCode == 39) this.key_right = true;
-		else if(event.keyCode == 37) this.key_left = true;
-		else if(event.keyCode == 38) this.key_up = true;
-		else if(event.keyCode == 40) this.key_down = true;
-	}
-
-	keyUp(event: KeyboardEvent) {
-		if(event.keyCode == 39) this.key_right = false;
-		else if(event.keyCode == 37) this.key_left = false;
-		else if(event.keyCode == 38) this.key_up = false;
-		else if(event.keyCode == 40) this.key_down = false;
-	}
-
-	update() {
-		let dx = 0;
-		let dy = 0;
-        let speed = this.model.turtle.speed;
-		if(this.key_right) dx += speed;
-		if(this.key_left) dx -= speed;
-		if(this.key_up) dy -= speed;
-		if(this.key_down) dy += speed;
-		if(dx != 0 || dy != 0)
-			this.model.move(dx, dy);
-	}
-
-	onAcknowledgeClick(ob: any) {
-		console.log(`Response to move: ${JSON.stringify(ob)}`);
-	}
-}
-
-
-class Game {
-	model: Model;
-    view: View;
-    controller: Controller;
-
-	constructor() {
-		this.model = new Model();
-		this.view = new View(this.model);
-		this.controller = new Controller(this.model, this.view);
-	}
-
-	onTimer() {
-		this.controller.update();
-		this.model.update();
-		this.view.update();
-	}
-}
-
-
-
 interface HttpPostCallback {
 	(x:any): any;
 }
 
-const random_id = (len:number) => {
-    let p = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    return [...Array(len)].reduce(a => a + p[Math.floor(Math.random() * p.length)], '');
-}
-
-const g_origin = new URL(window.location.href).origin;
-const g_id = random_id(12);
-
-// Payload is a marshaled (but not JSON-stringified) object
-// A JSON-parsed response object will be passed to the callback
 const httpPost = (page_name: string, payload: any, callback: HttpPostCallback) => {
 	let request = new XMLHttpRequest();
 	request.onreadystatechange = () => {
@@ -246,6 +166,132 @@ const httpPost = (page_name: string, payload: any, callback: HttpPostCallback) =
 	request.open('post', `${g_origin}/${page_name}`, true);
 	request.setRequestHeader('Content-Type', 'application/json');
 	request.send(JSON.stringify(payload));
+}
+
+
+class Controller
+{
+	model: Model;
+    view: View;
+    key_right: boolean = false;
+    key_left: boolean = false;
+    key_up: boolean = false;
+    key_down: boolean = false;
+	last_updates_request_time: number;
+
+	constructor(model: Model, view: View) {
+		this.model = model;
+		this.view = view;
+		this.key_right = false;
+		this.key_left = false;
+		this.key_up = false;
+		this.key_down = false;
+		this.last_updates_request_time = Date.now();
+		let self = this;
+		view.canvas.addEventListener("click", function(event) { self.onClick(event); });
+		document.addEventListener('keydown', function(event) { self.keyDown(event); }, false);
+		document.addEventListener('keyup', function(event) { self.keyUp(event); }, false);
+	}
+
+	onClick(event: MouseEvent) {
+		let x = event.pageX - this.view.canvas.offsetLeft;
+		let y = event.pageY - this.view.canvas.offsetTop;
+		this.model.onclick(x, y);
+
+		httpPost('ajax', {
+			id: g_id,
+			action: 'click',
+			x: x,
+			y: y,
+		}, this.onAcknowledgeClick);
+	}
+
+	keyDown(event: KeyboardEvent) {
+		if(event.keyCode == 39) this.key_right = true;
+		else if(event.keyCode == 37) this.key_left = true;
+		else if(event.keyCode == 38) this.key_up = true;
+		else if(event.keyCode == 40) this.key_down = true;
+	}
+
+	keyUp(event: KeyboardEvent) {
+		if(event.keyCode == 39) this.key_right = false;
+		else if(event.keyCode == 37) this.key_left = false;
+		else if(event.keyCode == 38) this.key_up = false;
+		else if(event.keyCode == 40) this.key_down = false;
+	}
+
+	on_receive_updates(ob:any) {
+		// console.log(`ob = ${JSON.stringify(ob)}`)
+
+		for (let i = 0; i < ob.updates.length; i++) {
+			let update = ob.updates[i];
+
+			let id = update[0];
+			let x = update[1];
+			let y = update[2];
+
+			let sprite = sprite_map[id];
+			if (sprite === undefined) {
+				let s = new Sprite(x, y, "green_robot.png", Sprite.prototype.go_toward_destination, Sprite.prototype.ignore_click);
+				s.set_destination(x,y);
+				this.model.sprites.push(s)
+				sprite_map[id] = s
+			} else {
+				sprite.set_destination(x,y);
+			}
+		}
+	}
+
+	request_updates() {
+		let payload = {
+			'id': g_id,
+			'action': 'update',
+		}
+
+		httpPost('ajax', payload, (ob) => {return this.on_receive_updates(ob)} );
+	}
+
+	update() {
+		let dx = 0;
+		let dy = 0;
+        let speed = this.model.character.speed;
+		if(this.key_right) dx += speed;
+		if(this.key_left) dx -= speed;
+		if(this.key_up) dy -= speed;
+		if(this.key_down) dy += speed;
+		if(dx != 0 || dy != 0)
+			this.model.move(dx, dy);
+
+		const time = Date.now();
+		let diff = time - this.last_updates_request_time;
+		if (diff >= 1000) {
+			this.last_updates_request_time = time;
+			this.request_updates();
+		}
+	}
+
+	onAcknowledgeClick(ob: any) {
+		// console.log(`Response to move: ${JSON.stringify(ob)}`);
+	}
+}
+
+
+class Game {
+	model: Model;
+    view: View;
+    controller: Controller;
+
+	constructor() {
+		this.model = new Model();
+		this.view = new View(this.model);
+		this.controller = new Controller(this.model, this.view);
+	}
+
+	onTimer() {
+		this.controller.update();
+		this.model.update();
+		this.view.update();
+	}
 }
 
 
